90d6ba943435df5f6a30df3cd7e9e24e78bc4e07
Mihail Stanchev <mihail.stanchev97@gmail.com>
2024-09-05 14:06:31 +0300

    Transformed the wall into a cube prefab and created a script to spawn them dynamically

--- Assets/Scripts/TheWall.cs
+++ Assets/Scripts/TheWall.cs
@@ -1,19 +1,58 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class TheWall : MonoBehaviour
 {
+    [SerializeField] GameObject wallCubePrefab;
+    [SerializeField] GameObject socketWallPrefab;
     [SerializeField] XRSocketInteractor wallSocket;
     [SerializeField] GameObject[] wallCubes;
+    [SerializeField] float cubeSpacing = 0.005f;
+
+
+    private Vector3 cubeSize;
+    private Vector3 spawPosition;
     void Start()
     {
-        if (wallSocket != null)
+        if (wallCubePrefab != null)
         {
-            wallSocket.selectEntered.AddListener(OnSocketEnter);
-            wallSocket.selectExited.AddListener(OnSocketExited);
+            cubeSize = wallCubePrefab.GetComponent<Renderer>().bounds.size;
+        }
+        cubeSize = wallCubePrefab.GetComponent<Renderer>().bounds.size;
+        spawPosition = transform.position;
+        BuildWall();
+    }
+
+    private void BuildWall()
+    {
+        wallCubes = new GameObject[2];
+        if (wallCubePrefab != null)
+        {
+            wallCubes[0] = Instantiate(wallCubePrefab, spawPosition, transform.rotation);
+        }
+
+        spawPosition.y += cubeSize.y + cubeSpacing;
+
+        if (socketWallPrefab != null)
+        {
+            wallCubes[1] = Instantiate(socketWallPrefab, spawPosition, transform.rotation);
+            wallSocket = wallCubes[1].GetComponentInChildren<XRSocketInteractor>();
+            if (wallSocket != null)
+            {
+                wallSocket.selectEntered.AddListener(OnSocketEnter);
+                wallSocket.selectExited.AddListener(OnSocketExited);
+            }
+        }
+        for (int i = 0; i < wallCubes.Length; i++)
+        {
+            if (wallCubes[i] != null)
+            {
+                wallCubes[i].transform.SetParent(transform);
+            }
         }
     }
 
