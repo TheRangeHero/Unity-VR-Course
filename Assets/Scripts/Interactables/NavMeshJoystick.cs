bddca79fee01aade5f3ec27f078a4cc20a271b48
Mihail Stanchev <mihail.stanchev97@gmail.com>
2024-09-19 23:29:49 +0300

    Added functionality to control the in-game robot with the joystick

--- Assets/Scripts/Interactables/NavMeshJoystick.cs
+++ Assets/Scripts/Interactables/NavMeshJoystick.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,37 @@ using UnityEngine;
 public class NavMeshJoystick : SimpleHindgeInteractable
 {
 
+    [SerializeField] NavMeshRobot robot;
     [SerializeField] Transform trackedObject;
     [SerializeField] Transform trackingObject;
+    [SerializeField] Transform rotationParentObject;
 
     protected override void ResetHinge()
     {
-
+        if (robot != null)
+        {
+            robot.StopAgent();
+        }
     }
 
     protected override void Update()
     {
         base.Update();
-        trackingObject.position = new Vector3(trackedObject.position.x, trackingObject.position.y, trackedObject.position.z);
+
+        if (isSelected)
+        {
+            MoveRobot();
+        }
+    }
+
+    private void MoveRobot()
+    {
+        if (robot != null)
+        {
+            trackingObject.position = new Vector3(trackedObject.position.x, trackingObject.position.y, trackedObject.position.z);
+            rotationParentObject.rotation = Quaternion.identity;
+            robot.MoveAgent(trackingObject.localPosition);
+        }
+
     }
 }
